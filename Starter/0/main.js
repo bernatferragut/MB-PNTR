457bfcf7a804906e958669bf19e7058712e0463a
bernat <elbernat@gmail.com>
2019-12-18 21:51:53 +0100

    update

---
+++ Starter/0/main.js
@@ -0,0 +1,15 @@
+// ON LOAD
+console.log('CONNECTED')
+
+// BODY
+let message =  document.querySelector('.app');
+
+console.log (message);
+
+let messageContent = 'CONTENT';
+
+message.innerHTML = `<pre>${messageContent}</pre>`;
+
+
+
+
