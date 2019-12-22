fc4d243e2accd7f5853ae84517f9fbbb8f8341b9
bernat <elbernat@gmail.com>
2019-12-22 20:19:47 +0100

    update

--- Starter/0/js/main.js
+++ Starter/0/js/main.js
@@ -6,24 +6,21 @@ window.onload = () => {
 // 2. DATA OBJECTS
 // GENERAL DATA
 let data = {
-    title : 'S°K°E°T°C°H',
-    PairButtonText : 'PAIR MICROBIT',
-    pairButtonCall : function() {
-        console.log('PAIRED!');
-    },
-    pairSuccessText : 'MICROBIT PAIRED',
     paired : false,
+    title1 : 'S°K°E°T°C°H',
+    pairButtonText : 'PAIR MICROBIT',
+    title2 : 'MICROBIT PAIRED',
 }
 // MICROBIT DATA
 let microBit = new uBit();
-console.log(microBit);
+console.log('microbit data: ', microBit);
 
 // 3. RENDER FUNCTIONS
-// TITLE
-function Title(data) {
+// TITLE1
+function Title1(data) {
     return `
     <div class="flex-container fadeIn">
-        <h1>${data.title}</h1>
+        <h1>${data.title1}</h1>
     </div>
     `
 }
@@ -34,66 +31,86 @@ function PairButton(data) {
         <button 
             class="btn" 
             onclick="searchDevice()">
-            ${data.PairButtonText}
+            ${data.pairButtonText}
         </button>
     </div>
     `
 }
-
-// PAIR-CONNECTION SUCCESS
-function Success(data) {
+// TITLE2
+function Title2(data) {
     return `
-    <div class"flex-container fedeIn">
-        <h1>${data.pairSuccessText}</h1>
+    <div class="flex-container fadeIn">
+        <h1>${data.title2}</h1>
+    </div>
+    `
+}
+// MICROBIT : SEARCH
+function searchDevice(){
+    console.log('...searching device')
+    microBit.searchDevice();
+}
+// ON CONNECTED
+microBit.onConnect(function(){
+    // Paired
+    data.paired = true;
+    console.log('microbit paired: ', data.paired);
+
+    Content(data);
+
+    document.querySelector('.app').innerHTML = Content(data);
+})
+
+// MB DATA
+function mbData() {
+    return `
+    <div class="flex-container fadeIn">
+        <h3>${microBit.getButtonA()}</h3>
+        <ul>
+            <li>${microBit.getAccelerometer().x}</li>
+            <li>${microBit.getAccelerometer().y}</li>
+            <li>${microBit.getAccelerometer().z}</li>
+        </ul>
+        <h3>${microBit.getButtonB()}</h3>
+    </div>
+    `
+}
+
+// BLE SUBSCRIBE SERVICE
+microBit.onBleNotify(function(){
+    data.paired = true;
+
+    mbData();
+
+})
+
+// CANVAS CREATION
+function CreateCanvas() {
+    console.log('Here we create the Canvas')
+    return`
+    <div class="flex-container fadeIn">
+        <canvas></canvas>
     </div>
     `
 }
 // MAIN CONTENT,
 function Content(data) {
-    return `
-    ${Title(data)}
-    ${PairButton(data)}
+
+    if(data.paired == true) {
+        return `
+        ${Title2(data)}
+        ${mbData()}
+        ${CreateCanvas()}
     `
-    // Do we need to concat these 2 strings?
+    } else {
+        return `
+        ${Title1(data)}
+        ${PairButton(data)}
+    `
+    }
 }
 
 // 4. HTML INJECTION
 document.querySelector('.app').innerHTML = Content(data);
 
-// 5. MICROBIT
-// SEARCH DEVICE
-function searchDevice(){
-    console.log('...searching device')
-    microBit.searchDevice();
-}
 
-// ON CONNECTED
-microBit.onConnect(function(){
-    // Paired
-    data.paired = true;
-    console.log(data.paired);
-    
-    if(data.paired == true) {
-        // get button and apply fadeout
-        // get div and append H1 - Connected
-        // for a specific time and fade out
-        // fadeIn x,y,z values 
-        // when buttons a/b blink
-        // add canvas to start drawing 
-    }
-
-    // HTML
-    // document.getElementById("connected").innerHTML="true";
-    // document.getElementById("properties").classList.toggle('inactive');
-    console.log("microbit connected!");
-
-    // Buttons
-    microBit.setButtonACallback(function(){
-        console.log("buttonA pressed");
-    });
-
-    microBit.setButtonBCallback(function(){
-        console.log("buttonB pressed");
-    });
-});
 
