ba4427ef6bc994a174bc0f8d0c18afa47dfca0a9
bernat <elbernat@gmail.com>
2020-01-03 16:26:43 +0100

    update

--- Dev/laser-paint-v2/src/routes/index.svelte
+++ Dev/laser-paint-v2/src/routes/index.svelte
@@ -1,13 +1,17 @@
 <script>
-	import { quintOut } from 'svelte/easing';
-	import { fade, draw, fly } from 'svelte/transition';
-
-	let visible = true;
 
 </script>
 
 
 <style>
+	@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono&display=swap');
+
+	/* VARIABupdateLES */
+	:root {
+		--main-bg-color: #101010;
+		--main-text-color: greenyellow;
+	}
+
 	.flex-container {
 		display: flex;
 		align-items: center; 
@@ -16,12 +20,35 @@
 		max-height: auto;
 	}
 
-	h1 {
-		color: var(--main-text-color);
+	.centered {
 		font-family: 'IBM Plex Mono', monospace;
-		font-size: 12px;
 		background-color: var(--main-bg-color);
-		margin: 10px;
+		color: var(--main-text-color);
+		padding: 0;
+		margin-top: 10vh;
+		font-size: 6vw;
+		letter-spacing: 1.12em;
+	}
+
+	  /* FADE IN  DESIGN */
+	@-webkit-keyframes fadeIn {
+		0% {opacity: 0;} to {opacity: 1;}
+	}
+	@keyframes fadeIn {
+		0% {opacity: 0;} to {opacity: 1;}
+	}
+
+	.fadeIn {
+	opacity:0;
+
+	animation: fadeIn ease-in 1;
+	-webkit-animation: fadeIn ease-in 1;
+
+	animation-fill-mode: forwards;
+	-webkit-animation-fill-mode: forwards;
+
+	animation-duration: 1.2s;
+	-webkit-animation-duration: 0.7s;
 	}
 
 </style>
@@ -32,17 +59,7 @@
 
 
 <div class="flex-container">
-	<h1>WELCOME</h1>
-	{#if visible}
-	<div class="centered">
-		{#each 'LAZERBIT' as char, i}
-			<span
-				in:fade="{{delay: 1000 + i * 150, duration: 800}}"
-			>{char}</span>
-		{/each}
-	</div>
-{/if}
-	
+	<div  class="centered fadeIn">LAZERBIT</div>
 </div>
 
 
