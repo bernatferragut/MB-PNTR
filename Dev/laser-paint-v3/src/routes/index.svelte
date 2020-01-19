00155fe0f4fb58536be2fb644d045647a62522dc
bernat <elbernat@gmail.com>
2020-01-19 12:28:10 -0500

    update

---
+++ Dev/laser-paint-v3/src/routes/index.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	let visible = false;
+
+	onMount(() => {
+		visible = true
+	});
+
+</script>
+
+
+<style>
+	@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono&display=swap');
+
+	/* VARIABLES*/
+	:root {
+		--main-bg-color: #101010;
+		--main-text-color: greenyellow;
+		--second-text-color:tomato;
+	}
+
+	.flex-container {
+		display: flex;
+		align-items: center; 
+		justify-content: center;
+		flex-direction: row;
+		max-height: auto;
+	}
+
+	.centered {
+		font-family: 'IBM Plex Mono', monospace;
+		background-color: var(--main-bg-color);
+		color: var(--main-text-color);
+		margin-top: 23vh;
+		width:100px;height:100px;
+		display:table-cell;
+		text-align:center;
+		vertical-align:middle;
+		font-size: 5vh;
+		letter-spacing: 5vw;
+	}
+</style>
+
+<svelte:head>
+	<title>HOME</title>
+</svelte:head>
+
+{#if visible}
+	<!-- GREEN LZR TEXT -->
+	<div class="flex-container">
+		{#each ' L Z R B I T' as char, i}
+			<div class="centered" in:fade="{{delay: 234 + i * 150, duration: 800}}">
+				{char}
+			</div>
+		{/each}
+	</div>
+{/if}
+
+
+
+
