3b9c6bffc8aa06c7d5409a80c9e70b020ffd0781
bernat <elbernat@gmail.com>
2020-01-26 15:02:23 -0500

    update

--- Dev/laser-paint-v3/lzrbit-vanilla/js/main.js
+++ Dev/laser-paint-v3/lzrbit-vanilla/js/main.js
@@ -1,5 +1,6 @@
-// JS
-const PARAMS = {
+console.log('1.js-connected');
+//////////////// CONTROL PARAMS ///////////
+let PARAMS = {
     acc : {x : 0, y : 0},
     line : false,
     lineWidth : 0.1,
@@ -8,14 +9,13 @@ const PARAMS = {
     dotWidth : 0.25,
     dotColor : {r: 255, g: 99, b: 71},
 }
+//////////////// CONTROL PARAMS ///////////
 
-//// TWEAKPANE /////
+//////////////// TWEAKPANE ////////////////
 // TWEAKPANE - DOT
 const paneDot = new Tweakpane({
     container: document.getElementById('dot'),
-    // title: 'Dot Parameters',
 });
-// console.log(paneLine);
 paneDot.addInput(PARAMS, 'dot', {label : 'DOT'});
 paneDot.addInput(PARAMS, 'dotWidth', {
     min : 0,
@@ -23,6 +23,10 @@ paneDot.addInput(PARAMS, 'dotWidth', {
     label : 'DOT WIDTH'
 });
 paneDot.addInput(PARAMS, 'dotColor', {label : 'DOT COLOR'});
+// panelDot - CHANGES
+paneDot.on('change', (value) => {
+    console.log( 'dot: ', value);
+})
 
 // TWEAKPANE - LINE
 const paneLine = new Tweakpane({
@@ -36,6 +40,10 @@ paneLine.addInput(PARAMS, 'lineWidth', {
     label : 'LINE WIDTH'
 });
 paneLine.addInput(PARAMS, 'lineColor', {label : 'LINE COLOR'});
+// panelLine - CHANGES
+paneLine.on('change', (value) => {
+    console.log( 'line: ', value);
+})
 
 // TWEAKPANE - ACCELEROMETER
 const paneAcc = new Tweakpane({
@@ -43,10 +51,119 @@ const paneAcc = new Tweakpane({
     title: 'MICROBIT ACCELEROMETER',
 });
 paneAcc.addInput(PARAMS, 'acc',{ label: 'ACCEL (X,Y)'});
+// panelAcc - CHANGES
+paneAcc.on('change', (value) => {
+    console.log( 'accel: ', value);
+})
+
 // TWEAKPANE - BUTTON SAVE
 paneAcc.addButton({
     title: 'Save Galaxy',
-  }).on('click', (value) => {
-    console.log(value);
+  }).on('click', () => {
+    console.log('click detected');
+    saveDrawing();
   });
-  //// TWEAKPANE /////
+//////////////// TWEAKPANE ////////////////
+
+//////////////// MICROBIT PAIRING /////////
+const pairBtn =document.querySelector('.btn');
+pairBtn.addEventListener('click', microbitPairing);
+let microbit = new uBit();
+let isPaired = false;
+
+function microbitPairing() {
+    //1.search
+    microbit.searchDevice();
+    //2.connect
+    microbit.onConnect( ()=> {
+        isPaired = true;
+        console.log("isPpared: ", isPaired);
+    })
+    //3.ble subscription
+    microbit.onBleNotify(function(){
+        PARAMS.acc.x = microbit.getAccelerometer().x;
+        PARAMS.acc.y = microbit.getAccelerometer().y;
+        // PARAMS.acc(x,y)
+        // console.log(PARAMS.acc.x);
+        // console.log(PARAMS.acc.y);
+    })
+    //4. button disappear
+    pairBtn.disabled = true;
+    //5. start drawing
+    // start(PARAMS);
+}
+//////////////// MICROBIT PAIRING /////////
+
+//////////////// CANVAS ///////////////////
+const canvas = document.querySelector('canvas');
+const ctx =  canvas.getContext('2d');
+let w = canvas.width = window.innerWidth;
+let h = canvas.height = window.innerHeight;
+let brush = new Brush(ctx);
+console.log('Params:', PARAMS.acc.y)
+let frame;
+
+
+// Animation
+(function loop() {
+    // context composition: none, copy, destination-atop, destination-in, destination-out, destination-over, source-top, source-in, source-out, source-over, lighter, xor
+    ctx.globalCompositeOperation = "lighter";
+
+
+
+    // line drawing
+    if(PARAMS.line){
+        // Background Color
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, w , h);
+        // Drawing Dot
+        let mx = brush.mapValues(PARAMS.acc.x,-1024,1024,0,w);
+        let my = brush.mapValues(PARAMS.acc.y,-1024,1024,0,h);
+        brush.draw_line(mx, my, PARAMS.lineWidth, PARAMS.lineColor);
+        // dot drawing
+    } else if(PARAMS.dot){
+        // Background Color
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, w , h);
+        // Drawing Dot
+        let mx = brush.mapValues(PARAMS.acc.x,-1024,1024,0,w);
+        let my = brush.mapValues(PARAMS.acc.y,-1024,1024,0,h);
+        brush.draw_point(mx,my, PARAMS.dotWidth, PARAMS.dotColor);
+        // allows to start path from here without jumping
+        ctx.beginPath(); 
+    } else {
+        ctx.clearRect(0, 0, w, h);
+        // Drawing Axist
+        let mx = brush.mapValues(PARAMS.acc.x,-1024,1024,0,w);
+        let my = brush.mapValues(PARAMS.acc.y,-1024,1024,0,h);
+        // Draw axis
+        brush.draw_axis(mx,my, w, h);
+    }
+    // Loop
+    frame = requestAnimationFrame(loop);
+}());
+
+
+// resize canvas
+window.addEventListener('resize', resizeCanvas);
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resizeCanvas();
+
+// save drawing
+function saveDrawing() {
+    console.log('saveDrawing');
+    // let finalCanvas = document.querySelector('#canvas');
+    const a = document.createElement('a');
+    document.body.appendChild(a);
+    a.href = canvas.toDataURL('image/png',1);
+    a.download = 'canvas-image.png';
+    a.click();
+    document.body.removeChild(a);
+}
+//////////////// CANVAS ///////////////////
+
+
+
