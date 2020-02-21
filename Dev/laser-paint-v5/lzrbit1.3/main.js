03de96678f6b6aca763d0a041b0c0f4219e7262c
bernat <elbernat@gmail.com>
2020-02-20 20:54:34 -0500

    upload

--- Dev/laser-paint-v5/lzrbit1.3/main.js
+++ Dev/laser-paint-v5/lzrbit1.3/main.js
@@ -153,12 +153,11 @@ let h = (canvas1.height = window.innerHeight);
 let brush = new Brush(ctx1,ctx2);
 let frame, mx, my;
 let pointer = new Brush(ctx1,ctx2);
-
-// SAVING AN IMAGE TO AVOID WEIRD BUG LATER IN MAIN SAVE ***
+////////////// DOWNLOAD IMAGE PREP.//////////////
+console.log('downloading Galaxy');
 let a = document.createElement('a');
 document.body.appendChild(a);
 a.href = canvas1.toDataURL('image/png');
-a.download = 'canvas-image.png';
 
 // Animation will be done on the TOP layer ( 2 Canvas )
 (function loop() {
@@ -210,13 +209,9 @@ resizeCanvas();
 // save drawing ***
 function saveDrawing() {
 	////////////// DOWNLOAD IMAGE //////////////
-	console.log('downloading Galaxy');
-	let a = document.createElement('a');
-	document.body.appendChild(a);
-	a.href = canvas1.toDataURL('image/png', 1);
 	a.download = 'canvas-image.png';
 	a.click();
-	document.body.removeChild(a);
+	// document.body.removeChild(a);
 	////////////// FIREBASE UPLOAD IMAGE //////////////
 	// image BLOB
 	let imgData = dataURLtoBlob(a.href);
@@ -288,6 +283,3 @@ document.addEventListener('DOMContentLoaded', function() {
 	});
 });
 //////////////// SLIDE-PANEL //////////////
-
-
-
