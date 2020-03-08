7dbe663cad6139995de5739093c916578d8b9720
bernat <elbernat@gmail.com>
2020-03-08 11:49:41 -0400

    update

--- Dev/laser-paint-v5/lzrbit1.3/brush.js
+++ Dev/laser-paint-v5/lzrbit1.3/brush.js
@@ -9,6 +9,47 @@ class Brush {
 		this.start_y = window.innerHeight/2;
 	}
 	// class methods
+	drawDot(mx, my, size, color) {
+		// style
+		this.context1.fillStyle = color;
+		// dot
+		this.context1.fillRect(mx, my, size,size);
+	}
+	drawLine(mx, my, size, color){
+		// style
+		this.context1.strokeStyle = color;
+		this.context1.lineWidth = size;
+		this.context1.lineCap = 'round'
+		// line
+		this.context1.lineTo(mx,my);
+		this.context1.stroke();
+		this.context1.beginPath();
+		this.context1.moveTo(mx,my);
+	}
+	drawCurve(mx, my, size, color,counter,pointsList){
+		// style
+		this.context1.strokeStyle = color;
+		this.context1.lineWidth = size;
+		this.context1.lineCap = 'round'
+		// we pass constantly 3 points list from main.js
+		if( counter === 3) {
+			// console.log( `counter: ${counter}, pointList: ${pointsList}`);
+			this.context1.beginPath();
+			// move to the starting point
+			this.context1.moveTo(this.start_x, this.start_y);
+			// bezier curve
+			this.context1.bezierCurveTo(pointsList[0][0], pointsList[0][1], pointsList[1][0], pointsList[1][1],mx, my);
+			this.context1.stroke();
+			// the last point will be the first in the next round
+			this.start_x = mx;
+			this.start_y = my;
+		}
+	}
+	// mapping microbit values to screen sizes
+	mapValues(n, start1, stop1, start2, stop2) {
+		return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
+	}
+	// AXIS
 	drawAxis(mx,my,w,h) {
 		// Background Color
 		ctx2.clearRect(0, 0, w, h);
@@ -24,47 +65,5 @@ class Brush {
 		ctx2.lineWidth = 1;
 		ctx2.strokeStyle = '#ff6347';
 		ctx2.stroke();
-	}
-	drawLine(mx, my, size, color){
-		// style
-		this.context1.strokeStyle = color;
-		this.context1.lineWidth = size;
-		this.context1.lineCap = 'round'
-		// line
-		this.context1.lineTo(mx,my);
-		this.context1.stroke();
-		this.context1.beginPath();
-		this.context1.moveTo(mx,my);
-	}
-
-	drawDot(mx, my, size, color) {
-		// style
-		this.context1.fillStyle = color;
-		// dot
-		this.context1.fillRect(mx, my, size,size);
-	}
-	drawCurve(mx, my, size, color,counter,pointsList){
-		// style
-		this.context1.strokeStyle = color;
-		this.context1.lineWidth = size;
-		this.context1.lineCap = 'round'
-		// we pass constantly 3 points list from main.js
-		if( counter === 3) {
-			// console.log( `counter: ${counter}, pointList: ${pointsList}`);
-			this.context1.beginPath();
-			// move to the starting point
-			this.context1.moveTo(this.start_x, this.start_y);
-			// bezier curve
-			this.context1.bezierCurveTo(pointsList[1][0], pointsList[1][1], pointsList[2][0], pointsList[2][1],mx, my);
-			this.context1.stroke();
-			// the last point will be the first in the next round
-			this.start_x = mx;
-			this.start_y = my;
-		}
-	}
-
-	// mapping microbit values to screen sizes
-	mapValues(n, start1, stop1, start2, stop2) {
-		return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
 	}
 }
